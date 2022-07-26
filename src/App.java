adfb2c681cf3cbc5d161f2f5bcbb787ff5c76515
unknown <penpenetai@icloud.com>
2022-07-26 14:47:31 -0700

    second

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Ultimate Iwatani Quest");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
